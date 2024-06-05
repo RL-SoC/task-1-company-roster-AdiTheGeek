63ce2d4ea87917cf02e91a54da0441f4b3ce4134
AdiTheGeek <155165784+AdiTheGeek@users.noreply.github.com>
2024-06-05 22:22:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,23 +14,36 @@ if __name__ == "__main__":  # Equivalent to int main() {} in C++.
 
         if last_input == 1:
             name = input("Name:")
+            age = input("Age:")
             ID = input("ID:")
             city = input("City:")
             branchcodes = input("Branch(es):")
             # How will you conver this to a list, given that
             # the user will always enter a comma separated list of branch codes?
             # eg>   2,5
-
-            salary = input("Salary: ")
+            branchcodes = [int(inp.strip()) for inp in branchcodes.split(",")]
+            position = input("Position:")
+            salary = input("Salary:")
             # Create a new Engineer with given details.
-            engineer = None # Change this
+            engineer = Engineer(name,age,ID,city,branchcodes,position,salary) # Change this
 
             engineer_roster.append(engineer) # Add him to the list! See people.py for definiton
             
         
         elif last_input == 2:
-            # Gather input to create a Salesperson
-            # Then add them to the roster
+            name = input("Name:")
+            age = input("Age:")
+            ID = input("ID:")
+            city = input("City:")
+            branchcodes = input("Branch(es):")
+            branchcodes = [int(inp.strip()) for inp in branchcodes.split(",")]
+            position = input("Position:")
+            superior = input("Enter superior ID:")
+            salary = input("Salary:")
+            # Create a new Salesperson with given details.
+            salesman = Salesman(name,age,ID,city,branchcodes,position,superior,salary) 
+
+            sales_roster.append(salesman) # Add him to the list! See people.py for definiton
             pass
 
         elif last_input == 3:
@@ -52,8 +65,9 @@ if __name__ == "__main__":  # Equivalent to int main() {} in C++.
                 ## which the employee reports as a comma separated list
                 ## eg> Branches: Goregaon,Fort
                 branch_names = []
-                ## ???? what comes here??
-                # print(f"Branches: " + ???? )
+                for i in found_employee.branchcodes:
+                    branch_names.append(branchmap[i]["name"])
+                print(f"Branches: " + "{i}," for i in branch_names )
                 
                 print(f"Salary: {found_employee.salary}")
 
@@ -62,42 +76,65 @@ if __name__ == "__main__":  # Equivalent to int main() {} in C++.
             # Change branch to new branch or add a new branch depending on class
             # Inheritance should automatically do this. 
             # There should be no IF-ELSE or ternary operators in this zone
-            pass
+            ID = int(input("ID: "))
+            new_branch = int(input("Branch to migrate to: "))
+
+            found_employee = None
+            for employee in engineer_roster + sales_roster:
+                if employee.ID == int(ID):
+                    found_employee = employee
+                    break
+            
+            found_employee.migrate_branch(new_branch)
             #### NO IF ELSE ZONE ENDS #################################################
 
         elif last_input == 5:
             ID = int(input("Enter Employee ID to promote: "))
             # promote employee to next position
+            found_employee = None
+            for employee in engineer_roster + sales_roster:
+                if employee.ID == int(ID):
+                    found_employee = employee
+                    break
+            x = found_employee.pre_defined_positions.index(found_employee.position)
+            promotion_pos = found_employee.pre_defined_positions[x+1]
+            found_employee.promote(promotion_pos)
 
         elif last_input == 6:
             ID = int(input("Enter Employee ID to give increment: "))
+            amt = int(input("Enter the increment amount: "))
             # Increment salary of employee.
+            found_employee = None
+            for employee in engineer_roster + sales_roster:
+                if employee.ID == int(ID):
+                    found_employee = employee
+                    break
+            
+            found_employee.increment(amt)
         
         elif last_input == 7:
             ID = int(input("Enter Employee ID to find superior: "))
             # Print superior of the sales employee.
-        
+            found_employee = None
+            for employee in sales_roster:
+                if employee.ID == int(ID):
+                    found_employee = employee
+                    break
+            
+            found_employee.find_superior()
+
         elif last_input == 8:
             ID_E = int(input("Enter Employee ID to add superior: "))
             ID_S = int(input("Enter Employee ID of superior: "))
             # Add superior of a sales employee
 
+            found_employee = None
+            for employee in sales_roster:
+                if employee.ID == int(ID_S):
+                    found_employee = employee
+                    break
+            
+            found_employee.add_superior(ID_E)
+
         else:
             raise ValueError("No such query number defined")
-
-            
-            
-
-            
-
-
-            
-
-
-        
-
-
-
-
-
-
